97cb4e98f08843b346a6afe929f53a00aaccd7fd
Mitra-rk <119427404+Mitra-rk@users.noreply.github.com>
2023-02-01 13:05:24 +0330

    mistake

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import "./App.css";
 import WeatherCondiotion from "./WeatherCondition";
 import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function App() {
-  let [city, setCity] = useState("Paris");
+  let [city, setCity] = useState("");
+  let [ready, setReady] = useState(false);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -12,37 +14,55 @@ export default function App() {
   function takeCity(event) {
     setCity(event.target.value);
   }
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <div className="Container">
-          <div className="SearchEngine p-3">
-            <form onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="col-9 text">
-                  <input
-                    type="text"
-                    placeholder="Type a city.."
-                    className="textBox w-100"
-                    onChange={takeCity}
-                  ></input>
-                </div>
-                <div className="col-3">
-                  <input
-                    type="submit"
-                    value="Search"
-                    className="btn btn-primary w-100"
-                  ></input>
-                </div>
-              </div>
-            </form>
-          </div>
+  function search() {
+    let apiKey = "0t0f733f3454c9aobbda64f6025e69d0";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function cityChanged() {}
 
-          <div className="display">
-            <WeatherCondiotion />
+  function handleResponse(response) {
+    setReady(true);
+    console.log(response.data);
+  }
+
+  if (ready) {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="Container">
+            <div className="SearchEngine p-3">
+              <form onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-9 text">
+                    <input
+                      type="text"
+                      placeholder="Type a city.."
+                      className="textBox w-100"
+                      onChange={takeCity}
+                    ></input>
+                  </div>
+                  <div className="col-3">
+                    <input
+                      type="submit"
+                      value="Search"
+                      className="btn btn-primary w-100"
+                      onClick={cityChanged}
+                    ></input>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div className="display">
+              <WeatherCondiotion />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading ...";
+  }
 }
