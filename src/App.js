bbaca1afab5af607f4e285d95883aac2d817008f
Mitra-rk <119427404+Mitra-rk@users.noreply.github.com>
2023-02-02 12:23:04 +0330

    set axios and remove warning

--- src/App.js
+++ src/App.js
@@ -6,63 +6,58 @@ import axios from "axios";
 
 export default function App() {
   let [city, setCity] = useState("");
-  let [ready, setReady] = useState(false);
 
+  function handleResponse(response) {
+    
+   
+    
+  }
+  
   function handleSubmit(event) {
     event.preventDefault();
   }
   function takeCity(event) {
     setCity(event.target.value);
+  
   }
-  function search() {
-    let apiKey = "0t0f733f3454c9aobbda64f6025e69d0";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-  function cityChanged() {}
+  function sendCity() {
+     let apiKey = "0t0f733f3454c9aobbda64f6025e69d0";
+     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 
-  function handleResponse(response) {
-    setReady(true);
-    console.log(response.data);
+     axios.get(apiUrl).then(handleResponse);
   }
-
-  if (ready) {
-    return (
-      <div className="App">
-        <div className="wrapper">
-          <div className="Container">
-            <div className="SearchEngine p-3">
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-9 text">
-                    <input
-                      type="text"
-                      placeholder="Type a city.."
-                      className="textBox w-100"
-                      onChange={takeCity}
-                    ></input>
-                  </div>
-                  <div className="col-3">
-                    <input
-                      type="submit"
-                      value="Search"
-                      className="btn btn-primary w-100"
-                      onClick={cityChanged}
-                    ></input>
-                  </div>
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <div className="Container">
+          <div className="SearchEngine p-3">
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-9 text">
+                  <input
+                    type="text"
+                    placeholder="Type a city.."
+                    className="textBox w-100"
+                    onChange={takeCity}
+                  ></input>
                 </div>
-              </form>
-            </div>
+                <div className="col-3">
+                  <input
+                    onClick={sendCity}
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  ></input>
+                </div>
+              </div>
+            </form>
+          </div>
 
-            <div className="display">
-              <WeatherCondiotion />
-            </div>
+          <div className="display">
+            <WeatherCondiotion cityValue={city} />
           </div>
         </div>
       </div>
-    );
-  } else {
-    search();
-    return "Loading ...";
-  }
+    </div>
+  );
 }
--- src/WeatherCondition.js
+++ src/WeatherCondition.js
@@ -1,35 +1,46 @@
 import "./App.css";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Unit from "./Unit";
 
 
-export default function WeatherCondiotion() {
-  return (
-    <div className="WeatherCondiotion">
-      <div className="Current p-3">
-        <div className="cityTime">
-          <h1 id="city">New York</h1>
-          <p id="day">Last updated at:</p>
-          <p id="condition">Mostly Cloudy</p>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-2">
-              <img src="" alt="icon"></img>
-            </div>
-            <div className="col-5">
-              <Unit />
-            </div>
-            <div className="col-5">
-              <ul>
-                <li id="humid">Humidity: %</li>
-                <li id="wind">Wind: Km/h</li>
-              </ul>
+export default function WeatherCondiotion(props) {
+  let [ready, setReady] = useState(false);
+  
+  
+  function search() {
+   
+  }
+  if (ready) {
+    return (
+      <div className="WeatherCondiotion">
+        <div className="Current p-3">
+          <div className="cityTime">
+            <h1 id="city">New York</h1>
+            <p id="day">Last updated at:</p>
+            <p id="condition">Mostly Cloudy</p>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-2">
+                <img src="" alt="icon"></img>
+              </div>
+              <div className="col-5">
+                <Unit />
+              </div>
+              <div className="col-5">
+                <ul>
+                  <li id="humid">Humidity: %</li>
+                  <li id="wind">Wind: Km/h</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
